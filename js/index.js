ace3162ebed7257cd1d9e8580f2722c404b737bf
Bethuel <bethropolis@gmail.com>
2022-03-30 01:27:53 +0300

    gooo copilot

--- js/index.js
+++ js/index.js
@@ -1,40 +1,99 @@
 const app = new Vue({
     el: '#app',
-    data: {
+    data: {       
+        displayPop: JSON.parse(localStorage.getItem("pop")) || null,
+        jokesLiked: JSON.parse(localStorage.getItem("like")) || [],
+        likedSwitch: false,
         jokesList: [],
         jokesCount: 1,
         jokesLimit: 10,
         jokesCurrent: JSON.parse(localStorage.getItem('joke')),
         colors: ['#d32f2f ', '#ba68c8', '#3d5afe', '#00796b', '#03a9f4', '#9e9d24 ', '#5d4037'],
-        url: 'https://v2.jokeapi.dev/joke/Pun?amount=10'
+        url: 'https://v2.jokeapi.dev/joke/Any?amount=20'
+
     },
     methods: {
         getJokes: function () {
             let url = this.url;
             $.get(url, data => {
                 if (data.error) return alert("an error occured getting more jokes");
-                this.setCount();
                 this.jokesList = data.jokes;
+                this.jokesCurrent = this.jokesList[0];
             })
         },
+        isLimit: function () {
+            if (this.jokesCount >= this.jokesLimit - 1) return true;
+            if (this.jokesCount <= 0) return true;
+            return false;
+        },
+        reset: function () {
+            if (this.likedSwitch) return
+            this.getJokes();
+            setCount();
+
+        },
         nextJoke: function () {
-            if (this.jokesCount >= this.jokesLimit - 1) {
-                setCount();
-                this.getJokes();
-                return
-            }
-            this.jokesCount = this.jokesCount + 1;
+            if (this.isLimit()) return
+            this.jokesCount++;
         },
         setCount(c = 1) {
             this.jokesCount = c;
+        },
+        prevJoke: function () {
+            if (this.jokesCount <= 0) return
+            this.jokesCount = this.jokesCount - 1;
+        },
+        likeJoke: function () {
+            if (this.likedSwitch) {
+                this.jokesLiked.push(this.jokesCurrent);
+                localStorage.setItem("like", JSON.stringify(this.jokesLiked));
+                this.likedSwitch = false;
+                return
+            }
+            this.jokesLiked.push(this.jokesCurrent);
+            localStorage.setItem("like", JSON.stringify(this.jokesLiked));
+            this.likedSwitch = true;
         },
         changeColor: function (id = Math.floor(Math.random() * 6) + 1) {
             document.documentElement.style.setProperty('--main', this.colors[id]);
             document.querySelector('meta[name="theme-color"]').setAttribute('content', this.colors[id])
             document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]').setAttribute('content', this.colors[id])
+        },
+        closePop: function () {
+            this.displayPop = true;
+            localStorage.setItem("pop", JSON.stringify(true))
+        },
+        parse(data) {
+            return JSON.parse(data)
+        },
+        loadLiked: function (params) {
+            this.likedSwitch = true;
+            this.jokesList = this.jokesLiked;
+        },
+        loadJokes: function () {
+            this.likedSwitch = false;
+        },
+        stringify(data) {
+            return JSON.stringify(data)
+        },
+        store(key, data, type) {
+            value = null;
+            if (!type) value = this.stringify(data);
+            if (type) {
+                let a = this.jokesLiked.push(this.jokesCurrent);
+                value = this.stringify(this.jokesLiked);
+            }
+            localStorage.setItem(key, value)
         }
     },
     watch: {
+        likedSwitch: function (val) {
+            if (val) {
+                this.jokesList = this.jokesLiked;
+            } else {
+                this.jokesList = this.jokesList;
+            }
+        },
         jokesList: function (params) {
             setCount();
             this.jokesCurrent = this.jokesList[this.jokesCount];
@@ -50,6 +109,11 @@ const app = new Vue({
     },
     mounted() {
         this.getJokes();
+        if (!this.displayPop) {
+            setTimeout(function (params) {
+                $("#cls1").text("click anywhere to close");
+            }, 7000)
+        }
     },
 })
 function setCount(params) {
@@ -64,3 +128,21 @@ if ('serviceWorker' in navigator) {
             console.log('Service worker registration failed, error:', error);
         });
 }
+
+
+swiper.add({
+    onLeft() {
+        app.nextJoke();
+        console.log('You swiped left.');
+    },
+    onRight() {
+        app.prevJoke();
+        console.log('You swiped right.');
+    },
+    onUp() {
+        app.likeJoke();
+        console.log('You swiped up.');
+    }
+})
+
+
---
+++ js/swipper.js
@@ -0,0 +1,90 @@
+const Swipe = class {
+    constructor(options) {
+        this.xDown = null;
+        this.yDown = null;
+
+        this.options = options;
+
+        this.handleTouchStart = this.handleTouchStart.bind(this);
+        this.handleTouchMove = this.handleTouchMove.bind(this);
+
+        document.addEventListener('touchstart', this.handleTouchStart, false);
+        document.addEventListener('touchmove', this.handleTouchMove, false);
+
+    }
+    add(options){
+       this.options = options;
+    }
+    onLeft() {
+        this.checkLimit()
+        this.options.onLeft();
+    }
+
+    onRight() {
+        this.checkLimit()
+        this.options.onRight();
+    }
+
+    onUp() {
+        this.options.onUp();
+    }
+
+    onDown() {
+        //this.options.onDown();
+    }
+    checkLimit(){
+        if (app.isLimit()) {
+            app.getJokes();            
+            app.setCount();
+            return
+        } 
+    }
+
+    static getTouches(evt) {
+        return evt.touches      // browser API
+
+    }
+
+    handleTouchStart(evt) {
+        const firstTouch = Swipe.getTouches(evt)[0];
+        this.xDown = firstTouch.clientX;
+        this.yDown = firstTouch.clientY;
+    }
+
+    handleTouchMove(evt) {
+        if (!this.xDown || !this.yDown) {
+            return;
+        }
+
+        let xUp = evt.touches[0].clientX;
+        let yUp = evt.touches[0].clientY;
+
+        let xDiff = this.xDown - xUp;
+        let yDiff = this.yDown - yUp;
+
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+            if (xDiff > 0 && this.options.onLeft) {
+                /* left swipe */
+                this.onLeft();
+            } else if (this.options.onRight) {
+                /* right swipe */
+                this.onRight();
+            }
+        } else {
+            if (yDiff > 0 && this.options.onUp) {
+                /* up swipe */
+                this.onUp();
+            } else if (this.options.onDown) {
+                /* down swipe */
+                this.onDown();
+            }
+        }
+
+        /* reset values */
+        this.xDown = null;
+        this.yDown = null;
+    }
+}
+
+let swiper = new Swipe();
